2e9692073b585bcdc5b38c1538ad221b964e16d1
Rasheev <rashif.aunur@ui.ac.id>
2023-09-20 14:26:37 +0700

    Selesai mengerjakan tutorial 3

--- main/views.py
+++ main/views.py
@@ -14,10 +14,10 @@ from main.models import Product
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
 
     context = {
-        'name': 'Rashif Aunur Rasyid',
+        'name': request.user.username,
         'class': 'PBP B',
         'products': products,
         'last_login': request.COOKIES['last_login'],
@@ -31,7 +31,9 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
