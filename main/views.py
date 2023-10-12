3fa7ba6099c915e28ee2168d7eda5300cae78a0f
Rasheev <rashif.aunur@ui.ac.id>
2023-10-12 21:47:05 +0700

    Selesai mengerjakan tutorial 4

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.core import serializers
 from django.shortcuts import render, redirect
 from main.forms import ProductForm
@@ -7,7 +7,8 @@ from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages  
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 from main.models import Product
 
@@ -112,3 +113,24 @@ def delete_product(request, id):
     product = Product.objects.get(pk=id)
     product.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponseRedirect(serializers.serialize('json', product_item))
+
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
